d229db1f87b3d311783dca5d61af6737e2509bf7
ksc0116 <101051124+ksc0116@users.noreply.github.com>
2025-06-02 17:59:20 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 34.34 ms, Memory: 27.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(vector<long long> numbers)
+{
+	vector<long long> answer;
+
+	for (int i = 0; i < numbers.size(); ++i)
+	{
+		// 현재 수가 짝수라면 1더해서 정답에 추가
+		if (numbers[i] % 2 == 0)
+		{
+			answer.push_back(numbers[i] + 1);
+			continue;
+		}
+
+		// 현재 수가 홀수라면
+		long long j = 1LL;
+		while (((numbers[i] + 1) & (1LL << j)) == 0)
+		{
+			j++;
+		}
+		
+		numbers[i] |= (1LL << j);
+		long long tmp2 = (1L << (j - 1));
+		numbers[i] -= tmp2;
+		answer.push_back(numbers[i]);
+	}
+	return answer;
+}
