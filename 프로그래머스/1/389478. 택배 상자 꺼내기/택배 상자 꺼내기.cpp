df5be11b449efe9336065c3721fe7afe40e57889
ksc0116 <101051124+ksc0116@users.noreply.github.com>
2025-06-02 12:50:25 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
@@ -0,0 +1,65 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int w, int num)
+{
+	int answer = 0;
+	vector<vector<int>> box;
+
+	int row = 0;
+	int cnt = 1;
+	while (n >= cnt)
+	{
+		box.push_back(vector<int>(w, 0));
+		if (row % 2 == 0)
+		{
+			// 짝수 행
+			for (int i = 0; i < w; ++i)
+			{
+				if (cnt <= n)
+				{
+					box[row][i] = cnt;
+					cnt++;
+				}
+			}
+		}
+		else
+		{
+			// 홀수 행
+			for (int i = w - 1; i >= 0; --i)
+			{
+				if (cnt <= n)
+				{
+					box[row][i] = cnt;
+					cnt++;
+				}
+			}
+		}
+		row++;
+	}
+
+	int findRow = 0;
+	int findCol = 0;
+
+	for (int i = 0; i < box.size(); ++i)
+	{
+		for (int j = 0; j < box[0].size(); ++j)
+		{
+			if (box[i][j] == num)
+			{
+				findRow = i;
+				findCol = j;
+			}
+		}
+	}
+	for (int i = findRow; i < box.size(); ++i)
+	{
+		if (box[i][findCol] != 0)
+		{
+			answer++;
+		}
+	}
+	return answer;
+}
