1e8d1b6378b8029f3ff24aad38fd1181c1edf2a1
ksc0116 <101051124+ksc0116@users.noreply.github.com>
2025-06-06 10:55:12 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool Check(vector<int>& citations, int mid)
+{
+    auto iter = lower_bound(citations.begin(), citations.end() , mid);
+    if(iter == citations.end()) return false;
+    
+    int idx = iter - citations.begin();
+    if(citations.size() - idx >= mid) return true;
+    
+    return false;
+}
+
+int solution(vector<int> citations) 
+{
+    sort(citations.begin(), citations.end());
+    int n = citations.size();
+
+    int left = 0;
+    int right = 10000;
+    
+    // 111
+    // 10
+    // 11111100000
+    // 100
+    
+    while(left < right)
+    {
+        int mid = (left + right) / 2;
+        
+        if(Check(citations, mid))
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid;
+        }
+    }
+    
+    return left - 1;
+}
